037d466c1f0afb596bf5194b031251f5f788e011
Luis Armendariz <luis@geodynamics.org>
2008-10-09 20:39:51 +0000

    Added some comments to generate_cover.py script

--- visual/cover/generate_cover.py
+++ visual/cover/generate_cover.py
@@ -5,16 +5,17 @@ import conman_vtk as conman
 
 def main():
     
-    numsteps = 100
-    nelx = 256
-    nelz = 64
+    numsteps = 100          # number of steps in model
+    nelx = 256              # number of elements in x1 direction
+    nelz = 64               # number of elements in x2 direction
 
     nx = nelx + 1
     nz = nelz + 1
-    numlines = nx*nz + 2
+    numlines = nx*nz + 2    # account for the two header lines on each step
 
-    filename = 'temp'
+    filename = 'temp'       # name of conman temperature output file
     conman.splitfile(filename, numsteps, numlines)
+
     for i in xrange(numsteps):
         print 'Writing %d-th file' % i
         conman.write_vtk(infile='temp_%03d' % i, outfile='cover_%03d.vtk' % i)
