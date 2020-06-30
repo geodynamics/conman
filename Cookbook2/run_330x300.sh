b9a99ba481bec11aa5ca2cced683419ae066fdb9
thwbecker <thorstinski@gmail.com>
2020-06-30 15:17:16 -0500

    Changed reference back to GMT instead of GMT4.

--- Cookbook2/run_330x300.sh
+++ Cookbook2/run_330x300.sh
@@ -2,16 +2,16 @@
 ../conman < run.unifA330x300
 \rm -f xa? plot.unif?330x300.ps
 split -99633 temp.unifA330x300
-./plot_wedge_GMT.sh xab
+../graphics/plot_wedge_GMT.sh xab
 mv plot.ps plot.unifA330x300.ps
 ../conman < run.unifB330x300
 \rm -f xa?
 split -99633 temp.unifB330x300
-./plot_wedge_GMT.sh xab
+../graphics/plot_wedge_GMT.sh xab
 mv plot.ps plot.unifB330x300.ps
 ../conman < run.unifC330x300
 \rm -f xa?
 split -99633 temp.unifC330x300
-./plot_wedge_GMT.sh xab
+../graphics/plot_wedge_GMT.sh xab
 mv plot.ps plot.unifC330x300.ps
 \rm -f xa?
--- Cookbook2/run_66x60.sh
+++ Cookbook2/run_66x60.sh
@@ -2,16 +2,16 @@
 ../conman < run.unifA66x60
 \rm -f xa? plot.unif?66x60.ps
 split -4089 temp.unifA66x60
-./plot_wedge_GMT.sh xab
+../graphics/plot_wedge_GMT.sh xab
 mv plot.ps plot.unifA66x60.ps
 ../conman < run.unifB66x60
 \rm -f xa? 
 split -4089 temp.unifB66x60
-./plot_wedge_GMT.sh xab
+../graphics/plot_wedge_GMT.sh xab
 mv plot.ps plot.unifB66x60.ps
 ../conman < run.unifC66x60
 \rm -f xa? 
 split -4089 temp.unifC66x60
-./plot_wedge_GMT.sh xab
+../graphics/plot_wedge_GMT.sh xab
 mv plot.ps plot.unifC66x60.ps
 \rm -f xa?
