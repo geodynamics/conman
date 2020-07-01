0d69f03cf2c4dbef9da98dad6fd8c6312dbb43e9
Scott King <sdk@vt.edu>
2020-07-01 11:41:49 -0400

    return scripts to use ../graphics version of script

--- Cookbook2/run_132x120.sh
+++ Cookbook2/run_132x120.sh
@@ -2,16 +2,16 @@
 ../conman < run.unifA132x120
 \rm -f xa? plot.unif?132x120.ps
 split -16095 temp.unifA132x120
-./plot_wedge_GMT.sh xab
+../graphics/plot_wedge_GMT.sh xab
 mv plot.ps plot.unifA132x120.ps
 ../conman < run.unifB132x120
 \rm -f xa? 
 split -16095 temp.unifB132x120
-./plot_wedge_GMT.sh xab
+../graphics/plot_wedge_GMT.sh xab
 mv plot.ps plot.unifB132x120.ps
 ../conman < run.unifC132x120
 \rm -f xa? 
 split -16095 temp.unifC132x120
-./plot_wedge_GMT.sh xab
+../graphics/plot_wedge_GMT.sh xab
 mv plot.ps plot.unifC132x120.ps
 \rm -f xa?
